8aaa6209d0131fc0716c034182a472a3a5c5c9ca
Ronjie Clear <ronjieclear@live.com>
2017-10-26 12:15:13 -0700

    string manipulate vid

---
+++ numbermanipulation.py
@@ -0,0 +1,59 @@
+#####  
+#num1 = 3
+#num2 = 3.14
+# print(type(num1)) #-return int
+# print(type(num2)) #-return float
+#####
+
+###operators
+# Division:			3 / 2 		return 1.5
+# Floor Division: 	3 // 2 		returns 1 it disregard decimal point
+# Exponent: 		3 **2		return 9 - exponent not power 3*3=3x3   |3**3=3x3x3
+# Modulus:			3 % 2 		return remainder = 1
+###operators
+
+### MDAS  * / + - 
+
+### num functions
+# Absolute value	abs(-3)		return positive value
+# Round				round(#,2)	return two decimal value
+
+# num1 = 11
+# num2 = 3
+# result = num1 / num2
+# print(round(result,3))
+
+
+#comparison operators == 
+# num1 = 11
+# num2 = 3
+# # print(num1 == num2)  #return boolean 'bool' True or False without using IF
+# x = num1 == num2
+# print(type(x))
+
+##### CASTING   - int() float() str() list()
+# num1 = '100.12'
+# num2 = "200"
+# # num1 = int(num1)
+# num1 = float(num1)
+# num2 = int(num2)
+# print(num1+num2)
+##### casting string to number
+
+#### the print IF BUG
+# print (0.1 + 0.1 == 0.2) #- retur TRUE
+# print (0.1 + 0.2 == 0.3) #- retur FALSE this could be a result of continous decimal point
+# print (0.1 + 0.3 == 0.4) #- retur TRUE
+#### the print IF BUG
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ stringmanipulation.py
@@ -0,0 +1,40 @@
+message = "Hello\'s World" # """ create a multi line string seperated by enter"""
+print(message)
+#vartype= type(message)
+#print(vartype)
+#print(message[1:5]) #string contcatenate
+#print(message.count('l')) #count the number of character in the string
+#print(message.find('W')) #look if there is a character in the list & return location case sensitive
+#newMessage = message.replace('Hello\'s','Hi')
+#print(newMessage)
+
+#######string concatenation####
+# greeting = "Hello"
+# name = "Python"
+# GMessage = greeting+ ", "+name
+# print(GMessage)
+#######string concatenation####
+
+#######string concatenation with format####
+# greeting = "Hello"
+# name = "Python"
+# GMessage = '{}, {}. Welcome!'.format(greeting,name)
+# print(GMessage)
+#######string concatenation with format####
+
+#######string concatenation with f string####
+# greeting = "Hello"
+# name = "Python"
+# GMessage = f'{greeting}, {name.upper()}. Welcome!'  #builtin function is working
+# print(GMessage)
+#######string concatenation with f string####
+
+####the HELP##########
+#print(dir(message)) #-shows all available function in that object
+#print(help(str)) #description of all the method of that class
+#print(help(str.upper)) #specific description of the method of that class
+####the HELP##########
+
+
+
+
